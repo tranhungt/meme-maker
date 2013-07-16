b62ef7d2d88ffbf675b59a7706ae77625c548c91
Hung Tran <tranhungt1@gmail.com>
2013-07-15 23:34:19 -0700

    updates host name

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,9 @@ MemeMaker::Application.configure do
     :bucket => ENV['AWS_BUCKET'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  }
+  }, 
+  :s3_host_name => 's3-us-west-2.amazonaws.com',
+
 }
 
 end
