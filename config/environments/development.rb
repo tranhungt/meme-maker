3abe9b3882a3d46dd678e0115f3cc31f1227efd9
Hung Tran <tranhungt1@gmail.com>
2013-07-15 23:21:12 -0700

    sets deve.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,15 @@ MemeMaker::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+
+    config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
+
 end
